1a2c2106a746c6fb960b52665d2e0f649a9d8344
Sachin Mohan <sachinmohanfff+1@gmail.com>
2025-11-13 17:33:32 +0530

    cron comand

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule queue worker to process jobs every minute
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
