f47f4e8167c77dd0cdd48a7ae2effe9f2edf8cfc
Sachin Mohan <sachinmohanfff+1@gmail.com>
2026-03-12 16:26:23 +0530

    organizatin user fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'phone',
         'profile_image',
         'role_id',
+        'organization_id',
         'is_active',
         'last_login_at',
         'last_login_ip'
